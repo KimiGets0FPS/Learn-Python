075fd0a07b2cf74c83fec81cf58d38757f02ae5a
Kimi <zheweiwan@gmail.com>
2020-11-05 17:05:19 -0800

    another random time to commit just 1 single file

--- Password Related Things/Password Generator.py
+++ Password Related Things/Password Generator.py
@@ -2,14 +2,14 @@ import string
 from random import *
 
 
-askT = int(input("How many passwords do you want?? "))
-while askT > 0:
-    print("A password larger or equal to 10 is unpredictible!!!")
-    askd = int(input("How much digits do you want in your password?(number has to be larger than 10!!!) "))
-    if askd < 10:
-        break
-    characters = string.ascii_letters + string.punctuation + string.digits
-    password ="".join(choice(characters) for x in range (askd))
-    print(password)
-    askT = askT - 1
-# THIS IS THE BEST PASSWORD GENERATOR EVER!!!!!
+number_of_passwords = int(input("How many passwords do you want?? "))
+while number_of_passwords > 0:
+    print("A password larger or equal to 10 is unpredictable!!!")
+    nums_of_value = int(input("How much digits do you want in your password?(number has to be larger than 10!!!) "))
+    if nums_of_value > 10:
+        characters = string.ascii_letters + string.punctuation + string.digits
+        password = ''.join(choice(characters) for x in range(nums_of_value))
+        print(password)
+        askT = number_of_passwords - 1
+    else:
+        print("The number that you just inputted has to be bigger than 10!!")
