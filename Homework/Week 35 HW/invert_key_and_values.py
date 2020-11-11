0b1c3434d1d5146b9187cfb47290ab104a7bbd7b
Kimi <zheweiwan@gmail.com>
2020-11-11 15:56:59 -0800

    All homework completed!

--- Homework/Week 35 HW/invert_key_and_values.py
+++ Homework/Week 35 HW/invert_key_and_values.py
@@ -1,14 +1,28 @@
+import unittest
+
+
 def key_and_values(input_dict):
     """
-    >>> key_and_values({ 'z':'q', 'w':'f' })
-    { 'q':'z', 'f':'w' }
-    >>> key_and_values({ 'a': 1, 'b':2, 'c':3 })
-    {1:'a', 2:'b', 3:'c'}
+    >>> key_and_values({'z':'q', 'w':'f'})
+    {'q': 'z', 'f': 'w'}
+    >>> key_and_values({'a': 1, 'b':2, 'c':3})
+    {1: 'a', 2: 'b', 3: 'c'}
     """
-    for _ in input_dict:
-        pass
+    copy_of_dict = input_dict.copy()
+    input_dict.clear()
+    for key, value in copy_of_dict.items():
+        input_dict[value] = key
+    return input_dict
+
+
+
+class Test(unittest.TestCase):
+    def test_if_correct(self):
+        inverting = key_and_values({'a': 1, 'b': 2, 'c': 3})
+        self.assertTrue(inverting, {1: 'a', 2: 'b', 3: 'c'})
 
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod(verbose=True)
+    unittest.main()
--- Homework/Week 35 HW/sum.py
+++ Homework/Week 35 HW/sum.py
@@ -1,29 +1,43 @@
-# >>> sum_of_two(5125515215521515, 125261616261626)
-# 5250776831783141
-# >>> sum_of_two('6666666666666666666666666666', '99999999999999999999999')
-# '6666766666666666666666666665'
-# >>> sum_of_two('123456789123456789123456789', '987654321987654321987654329876543')
-# '987654445444443445444443453333332'
+import unittest
 
 
 def sum_of_two(num_1, num_2):
     """
-    >>> sum_of_two('123', '456')
-    '581'
+    >>> sum_of_two('5125515215521515', '125261616261626')
+    '5250776831783141'
+    >>> sum_of_two('6666666666666666666666666666', '99999999999999999999999')
+    '6666766666666666666666666665'
+    >>> sum_of_two('123456789123456789123456789', '987654321987654321987654329876543')
+    '987654445444443445444443453333332'
     """
     num_list_1 = list(num_1)
     num_list_2 = list(num_2)
     multiplier = 1
-    i = 0
+    i = -1
     for _ in num_list_1:
         num_list_1[i] = int(num_list_1[i])
         num_list_1[i] *= multiplier
-        num_list_2[i] = int(num_list_2[i])
-        num__list_2 *= multiplier
+        i -= 1
         multiplier *= 10
-    return sum(num_list_1, num__list_2)
+    multiplier = 1
+    i = -1
+    for _ in num_list_2:
+        num_list_2[i] = int(num_list_2[i])
+        num_list_2[i] *= multiplier
+        i -= 1
+        multiplier *= 10
+    sum_1 = sum(num_list_1)
+    sum_2 = sum(num_list_2)
+    return str(sum_1 + sum_2)
+
+
+class Test(unittest.TestCase):
+    def test_if_correct(self):
+        sum_of_num = sum_of_two('5125515215521515', '125261616261626')
+        self.assertEqual(sum_of_num, '5250776831783141')
 
 
 if __name__ == "__main__":
     import doctest
+    unittest.main()
     doctest.testmod(verbose=True)
