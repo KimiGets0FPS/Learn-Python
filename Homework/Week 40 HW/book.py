25a8edcfc96fc17f7a6616c9c09f3f48e327e76a
Kimi <65819715+SirCodesAIot@users.noreply.github.com>
2020-12-29 21:36:48 -0800

    more improved mehoinoy

--- Homework/Week 40 HW/book.py
+++ Homework/Week 40 HW/book.py
@@ -1,74 +1,80 @@
 class Book:
-    def __init__(self, books, availiable_books=[], lend_books=[], occupied_books=[]):
-        # books: the books in a library
+    def __init__(self, books, available_books=[], lend_books=[]):
+        #* books: the books in a library
         self.books = books
-        # books that are available
-        self.availiable_books = availiable_books
-        # lend_books: the books that the user lended
+        #* books that are available
+        self.available_books = available_books
+        #* lend_books: the books that the user lended
         self.lend_books = lend_books
-        # occupied_books: the unavailable books that other users borrwoed.
-        self.occupied_books = occupied_books
-
-    @property
-    def available_books(self):
-        temp = []
-        for i in self.availiable_books:
-                temp.append(i)
-        if len(self.books) > 10:
-            return f"Current available books: {', '.join(temp[:10])}..."
-        return f"Current available books: {', '.join(temp)}"
 
     #* See the specific details about a specific book
     def check_book(self, book):
         if book.title() in self.books:
-            book_price = self.books[book.title()]
-            if book.title() not in self.occupied_books:
-                return f"{book.title()}, by {book_price[0]}, is avaliable."
-            return f"{book.title()}, by {book_price[0]}, is not avaliable."
+            book_info = self.books[book.title()]
+            if book.title() not in self.lend_books:
+                return f"{book.title()} (costing {book_info[1]}), by {book_info[0]}, is avaliable."
+            return f"{book.title()} (costing {book_info[1]}), by {book_info[0]}, is not avaliable."
         return f"We don't have {book.title()} in this library."
 
     #! REQUIRES STAFF ACCESS
-    def add_book(self, book, book_price):
+    def add_book(self, book, book_price, book_author):
         if book.title() in self.books:
             return f'The book {book.title()} is already in the library.'
+        self.available_books.append(book.title())
         self.books[book.title()] = book_price
+        self.books[book.title()] = book_author
         return 'Book added.'
 
     #! REQUIRES STAFF ACCESS
     def delete_book(self, book, output=[]):
         if book.title() in self.books:
             del self.books[book.title()]
+            if book.title() in self.available_books:
+                self.available_books.remove(book.title())
+            elif book.title() in self.lend_books:
+                self.lend_books.remove(book.title())
             for i in self.books:
                 output.append(i)
-            if len(output) > 10:
-                output = output[:10]
-            return f"Books that are available: {', '.join(output)}"
-        return 'Not a valid book'
-    
-    def book_price(self, book):
-        if book.title() in self.books:
-            return f'The book {book.title()} costs {self.books[book.title()]} dollars'
+            return 'Book deleted'
         return 'Not a valid book'
 
-    def lend_book(self, book):
-        if book.title() not in self.occupied_books:
+    def borrow_book(self, book):
+        if book.title() in self.available_books:
             self.lend_books.append(book.title())
-            self.availiable_books.remove(book.title())
+            self.available_books.remove(book.title())
             return 'Book is available and you borrowed it'
         return 'The book is not avialable'
 
     def return_book(self, book):
         if book.title() in self.lend_books:
-            self.lend_books.remove([book.title()])
-            self.availiable_books.append(book.title())
+            self.lend_books.remove(book.title())
+            self.available_books.append(book.title())
+            return 'Book returned'
         return f"{book.title()} isn't in your lend books list."
 
+    #* The Current Avaliable Books
+    @property
+    def see_available_books(self):
+        if bool(self.available_books) == False:
+            return "There aren't any books available."
+        if len(self.available_books) > 10:
+            return f"Current available books: {', '.join(self.available_books[:10])}..."
+        return f"Current available books: {', '.join(self.available_books)}"
+
+    @property
+    def borrowed_book(self):
+        if bool(self.lend_books) == False:
+            return "You don't need to return any books."
+        if len(self.lend_books) > 10:
+            return f"{', '.join(self.lend_books[:10])}..."
+        return ', '.join(self.lend_books)
+
     # TODO: COMPLETE MODIFY BOOK FUNCTION
-    def modify(self, book, price='', author=''):
+    def modify(self, book, new_price='', new_author=''):
         if book.title() in self.books:
-            if price:
-                ...
-            if author:
-                ...
+            if new_price:
+                self.books[book.title()][1] = new_price
+            if new_author:
+                self.books[book.title()][0] = new_author
             return f"{book.title()} has been succesfully modified."
         return "Not a valid book"
--- Homework/Week 40 HW/main.py
+++ Homework/Week 40 HW/main.py
@@ -3,12 +3,11 @@ from book import Book
 
 
 # TODO: add modify book function
-user = User({'Brian': [1234, 13], 'Ryan': [9012, 13]}, {'Kimi': [5678, 14]})
+user = User({'Brian': ['1234', '13'], 'Ryan': ['9012', '13']}, {'Kimi': ['5678', '14']})
 #! adding more details to books
-booksetting = Book({'Harry Potter 1': ['J.K. Rowling', 13], 
-    'Harry Potter 2': ['J.K. Rowling', 14], 
-    'Harry Potter 3': ['J.K. Rowling', 20]}, 
-    ['Harry Potter 1'], ['Harry Potter 2'],  ['Harry Potter 3'])
+booksetting = Book({'Harry Potter 1': ['J.K. Rowling', '13'], 
+    'Harry Potter 2': ['J.K. Rowling', '14']}, 
+    ['Harry Potter 1'], ['Harry Potter 2'])
 
 
 class Main:
@@ -18,13 +17,14 @@ class Main:
         get_passw = input(f'What is your password: ')
         if user.signin(get_name, get_passw) == True or 'Staff':
             while True:
+                print('------------------------------------------')
                 userinpt = input(""
                     "1.) Borrow Books\n"
                     "2.) Return Books\n"
-                    "3.) See Current Avaliable books\n"
-                    "4.) Book Details\n"
-                    "5.) See How Much You Owe\n"
-                    "6.) Book price\n"
+                    "3.) See Current Available books\n"
+                    "4.) See Current books you need to return\n"
+                    "5.) Book Details\n"
+                    "6.) See How Much You Owe\n"
                     "7.) Add Book (Require staff access)\n"
                     "8.) Delete Book (Require staff access)\n"
                     "9.) Modify Book (Require staff access)\n"
@@ -33,26 +33,25 @@ class Main:
                 #! 1
                 if userinpt == '1':
                     gbook = input('What book do you want to borrow: ')
-                    print(booksetting.lend_book(gbook))
+                    print(booksetting.borrow_book(gbook))
                 #! 2
                 elif userinpt == '2':
                     rbook = input('What book do you want to return: ')
                     print(booksetting.return_book(rbook))
                 #! 3
                 elif userinpt == '3':
-                    print(booksetting.available_books)
+                    print(booksetting.see_available_books)
+                
                 #! 4
                 elif userinpt == '4':
-                    cbook = input("What book details do you want to see: ")
-                    print(booksetting.check_book(cbook))
+                    print(booksetting.borrowed_book)
                 #! 5
                 elif userinpt == '5':
-                    print(user.owe_money(get_name.title()))
+                    cbook = input("What book details do you want to see: ")
+                    print(booksetting.check_book(cbook))
                 #! 6
                 elif userinpt == '6':
-                    bprice = input("Enter the book that you want to see "
-                    "how much it costs: ")
-                    print(booksetting.book_price(bprice))
+                    print(user.owe_money(get_name.title()))
                 #! 7, 8, and 9
                 elif userinpt == '7' or userinpt == '8' or userinpt == '9':
                     # all requires admin access so it's better to make it together
@@ -63,26 +62,15 @@ class Main:
                             print('You are not a staff.')
                     else:
                         bbook = input('Book name: ')
-
                         if userinpt == '7':
                             bprice = input('Book price: ')
-                            print(booksetting.add_book(bbook, bprice))
+                            bauthor = input('Book author: ')
+                            print(booksetting.add_book(bbook, bprice, bauthor))
                         elif userinpt == '8':
                             print(booksetting.delete_book(bbook))
                         else:
-                            """choice = input(""
-                            "1.) Change Price ONLY\n"
-                            "2.) Change Author ONLY\n"
-                            "3.) Change both\n"
-                            "Your Answer (enter number)")
-                            gbook = input("Enter the book name you want to change: ")
-                            if choice == '1':
-                                gprice = input("Enter the new price for the book:")
-                            elif choice == '2':
-                                gauthor = input("Enter the new author for the book: ")
-                            elif choice == '3':"""
-                            gprice = input("Enter the book name you want to change: ")
-                            guathor = input("Enter the new author for the book: ")
+                            gprice = input('Enter the new price for the book: ')
+                            guathor = input('Enter the new author for the book: ')
                             print(booksetting.modify(bbook, gprice, guathor))
                 #! 10
                 elif userinpt == '10':
