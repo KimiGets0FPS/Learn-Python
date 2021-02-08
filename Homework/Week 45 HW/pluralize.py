606491c1ab487ff34866a7974b6b715ac6b66dad
Kimi <65819715+SirCodesAIot@users.noreply.github.com>
2021-02-08 15:05:22 -0800

    test

--- Homework/Week 45 HW/pluralize.py
+++ Homework/Week 45 HW/pluralize.py
@@ -1,20 +1,25 @@
+import unittest
+
+
 def pluralize(words: list) -> list:
-    """
-    >>> pluralize(["cow", "pig", "cow", "cow"])
-    ['cows', 'pig']
-    >>> pluralize(["table", "table", "table"])
-    ['tables']
-    """
     output = []
     for i in words:
-        if words.count(i) >= 2:
-            if i + 's' not in output:
-                output.append(i + 's')
-        else:
+        if words.count(i) < 2:
             output.append(i)
+        elif i + 's' not in output:
+            output.append(i + 's')
     return output
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+
+
+class Test(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(pluralize(["cow", "pig", "cow", "cow"]), ['cows', 'pig'])
+
+    def test_2(self):
+        self.assertEqual(pluralize(["table", "table", "table"]), ['tables'])
 
 
 if __name__ == '__main__':
-    import doctest
-    doctest.testmod(verbose=True)
+    unittest.main(verbosity=True)
