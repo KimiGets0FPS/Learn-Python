2ec027d12e93aa01ef20440d50355a8140ae85d0
Kimi <65819715+SirCodesAIot@users.noreply.github.com>
2021-02-13 14:29:56 -0800

    ALl homework done

--- Homework/Week 46 HW/duplicate_in_array.py
+++ Homework/Week 46 HW/duplicate_in_array.py
@@ -1,15 +1,16 @@
-def find_duplicate(array, keep=[]):
+def find_duplicate(array):
     """
     >>> find_duplicate([1, 2, 3, 2, 1])
     True
     """
+    keep = []
     for i in array:
         if i not in keep:
             return True
         keep.append(i)
     return False
     # Time complexity = O(n)
-    # Space complexity = O(2n-i) 2n - how many times it had to run.
+    # Space complexity = O(2n-i) --> 2n - how many times it had to run.
 
 
 if __name__ == '__main__':
