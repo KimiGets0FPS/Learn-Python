535201ff7b4feaa13f7ef749f6981664e479edf2
Kimi <65819715+SirCodesAIot@users.noreply.github.com>
2021-02-23 16:52:28 -0800

    Home work done boiiiis

---
+++ Homework/Week 48 HW/friday_13th.py
@@ -0,0 +1,17 @@
+import datetime
+
+
+def friday_the_13th(month, year):
+    """
+    >>> friday_the_13th(3, 2020)
+    True
+    >>> friday_the_13th(10, 2017)
+    True
+    """
+    return True if datetime.datetime.strptime(f"13 {month} {year}", "%d %m %Y").weekday() == 4 else False
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod(verbose=True)
