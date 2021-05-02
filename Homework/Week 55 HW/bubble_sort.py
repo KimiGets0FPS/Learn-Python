e269118cb1391b205877ec811d9e1d76e8e4d9e1
Kimi <65819715+SirCodesAIot@users.noreply.github.com>
2021-05-02 11:01:20 -0700

    better bubble sort

--- Homework/Week 55 HW/bubble_sort.py
+++ Homework/Week 55 HW/bubble_sort.py
@@ -3,10 +3,12 @@ def bubble_sort(nums: list[int]) -> list[int]:
     >>> bubble_sort([9, 6, 7, 4, 8, 3, 5, 2, 1])
     [1, 2, 3, 4, 5, 6, 7, 8, 9]
     """
+    limit = 0
     for i in range(len(nums)):
-        for j in range(len(nums)):
+        for j in range(len(nums)-1):
             if nums[i] < nums[j]:
                 nums[i], nums[j] = nums[j], nums[i]
+        limit += 1
     return nums
 
 
